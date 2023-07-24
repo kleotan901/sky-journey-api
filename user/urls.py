4319e12458c921cbce7df62c14f063ed98f46736
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-24 12:28:45 +0200

    Fixed paths to endpoints in urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from user.views import UserViewSet, CreateUserView, ManageUserView
+from user.views import CreateUserView, ManageUserView
 
 app_name = "user"
-router = routers.DefaultRouter()
-router.register("passengers", UserViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)),
     path("register/", CreateUserView.as_view(), name="create"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
