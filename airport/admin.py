a02cbb7fb88bdfd736230145c469e43bac5c6f8c
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-24 12:29:56 +0200

    Registered models in admin.py

--- airport/admin.py
+++ airport/admin.py
@@ -14,6 +14,16 @@ from airport.models import (
 )
 
 
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
+
 admin.site.register(Crew)
 admin.site.register(Country)
 admin.site.register(City)
@@ -22,5 +32,3 @@ admin.site.register(AirplaneType)
 admin.site.register(Airplane)
 admin.site.register(Airport)
 admin.site.register(Flight)
-admin.site.register(Ticket)
-admin.site.register(Order)
