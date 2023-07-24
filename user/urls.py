d7c962afe56e3fdb9edb6cd4040a35df43d6d641
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-24 12:26:06 +0200

    Added drf and implemented CreateUserView & ManageUserView

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-app_name = "user"
+from user.views import UserViewSet, CreateUserView, ManageUserView
 
-urlpatterns = []
+app_name = "user"
+router = routers.DefaultRouter()
+router.register("passengers", UserViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, generics
 
-# Create your views here.
+from user.models import User
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
