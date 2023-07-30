ee621a8a881b48bc63e054219089abe7dbe04709
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-30 16:08:44 +0200

    Fixed send_email function - ACTIVATION_LINK moved to settings.py

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -177,3 +177,4 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+ACTIVATION_LINK = "http://127.0.0.1:8000/api/user/email-verification?token_id="
