894d9ea7c50c8df46bd9874e6678c9347b7ffa20
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-30 12:08:06 +0200

    Added is_email_confirmed field to user serializer

--- user/serializers.py
+++ user/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
             "email",
             "password",
             "first_name",
-            "last_name",
+            "last_name"
         )
         read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
@@ -33,4 +33,6 @@ class UserSerializer(serializers.ModelSerializer):
 class UserListSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "first_name", "last_name")
+        fields = (
+            "id", "email", "first_name", "last_name", "is_email_confirmed"
+        )
