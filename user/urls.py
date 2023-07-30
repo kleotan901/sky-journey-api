aa5bc4eeabcf8e0f1f240fc3c734f79538230b18
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-30 12:09:57 +0200

    Added email-confirmation/ & email-verification/ endpoints to urls.py

--- user/urls.py
+++ user/urls.py
@@ -5,14 +5,24 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from user.views import CreateUserView, ManageUserView
+from user.views import CreateUserView, ManageUserView, SendEmailConfirmationAPIView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path(
+        "email-confirmation/",
+        SendEmailConfirmationAPIView.as_view({"post": "post"}),
+        name="email-confirmation",
+    ),
+    path(
+        "email-verification/",
+        SendEmailConfirmationAPIView.as_view({"post": "email_verification"}),
+        name="email-verification",
+    ),
+    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token-verify"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
