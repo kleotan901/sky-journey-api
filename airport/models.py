8d9c6cc6e276ec453f9fb9d150670c1a638b1b06
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-30 16:05:13 +0200

    Fixed order of inner classes, methods & attributes in models.py

--- airport/models.py
+++ airport/models.py
@@ -7,11 +7,11 @@ class Crew(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
-
     class Meta:
         ordering = ["last_name"]
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
 
 
 class Country(models.Model):
@@ -80,12 +80,12 @@ class Airplane(models.Model):
     class Meta:
         verbose_name_plural = "airplanes"
 
+    def __str__(self) -> str:
+        return self.name
+
     @property
     def capacity(self) -> int:
         return self.rows * self.seats_in_row
-
-    def __str__(self) -> str:
-        return self.name
 
 
 class Flight(models.Model):
@@ -105,11 +105,11 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    def __str__(self) -> str:
-        return str(self.created_at)
-
     class Meta:
         ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return str(self.created_at)
 
 
 class Ticket(models.Model):
@@ -117,6 +117,23 @@ class Ticket(models.Model):
     seat = models.IntegerField()
     flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name="tickets")
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="tickets")
+
+    class Meta:
+        unique_together = ("flight", "row", "seat")
+        ordering = ["row", "seat"]
+
+    def __str__(self):
+        return f"{str(self.flight)} (row: {self.row}, seat: {self.seat})"
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        super(Ticket, self).save(force_insert, force_update, using, update_fields)
 
     @staticmethod
     def validate_seat(
@@ -138,20 +155,3 @@ class Ticket(models.Model):
         Ticket.validate_seat(
             self.flight, self.seat, "seat", "seats_in_row", ValidationError
         )
-
-    def save(
-        self,
-        force_insert=False,
-        force_update=False,
-        using=None,
-        update_fields=None,
-    ):
-        self.full_clean()
-        super(Ticket, self).save(force_insert, force_update, using, update_fields)
-
-    def __str__(self):
-        return f"{str(self.flight)} (row: {self.row}, seat: {self.seat})"
-
-    class Meta:
-        unique_together = ("flight", "row", "seat")
-        ordering = ["row", "seat"]
