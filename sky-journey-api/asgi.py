be935f2907c0def42929205645e5ea68eb20ee8d
Tetiana Kozakova <kleotan901@gmail.com>
2024-07-03 10:42:18 +0300

    Renamed project from airport-api-service to sky-journey-api

--- sky-journey-api/asgi.py
+++ sky-journey-api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for airport_api_service project.
+ASGI config for sky-journey-api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "airport_api_service.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sky-journey-api.settings")
 
 application = get_asgi_application()
